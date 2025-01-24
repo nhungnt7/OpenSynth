4d16bdeac482bfc87bdc3760fe90e48579653dfb
ntnhung7 <nguyennhung0799@gmail.com>
2025-01-24 15:24:34 +0700

    add data examples

--- src/response_generation/synthesize_responses.py
+++ src/response_generation/synthesize_responses.py
@@ -56,7 +56,7 @@ async def process_chunk(
 async def synthesize_response(file_name, batch_size=settings.CONF['batch_size']):
     openai_client = OpenAIGenerator(settings.CONF['llm'])
     filtering_metrics = FilteringMetrics()
-    instructions_file = os.path.join(settings.CONF['data']['instrutions'], file_name)
+    instructions_file = os.path.join(settings.CONF['data']['instructions'], file_name)
     responses_file = os.path.join(settings.CONF['data']['responses'], file_name)
 
     # Track processed chunks
